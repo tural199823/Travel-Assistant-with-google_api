3edbdc52c890f1a3b40018659b388efd1160cf4f
tural199823 <149156477+tural199823@users.noreply.github.com>
2024-07-03 23:32:24 +0200

    Add files via upload

--- testing.py
+++ testing.py
@@ -1,6 +1,8 @@
+from google_api_search import travel_assistant
+
 lattitude=53.549438522200916
 longtitude=9.985065094736022
-from google_api_search import travel_assistant
+
 if __name__ == "__main__":
-    travel_assistant(lat=lattitude, lng=longtitude, topics="pizza, restaurants", total_n=15)
+    travel_assistant(lat=lattitude, lng=longtitude, topics="pizza, restaurants", total_n=5)
 
