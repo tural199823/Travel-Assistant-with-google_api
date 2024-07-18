239035e2cd9d6ec185df9f172fbbf5c183010719
tural199823 <149156477+tural199823@users.noreply.github.com>
2024-07-18 17:04:59 +0200

    Delete testing.py

--- testing.py
+++
@@ -1,8 +0,0 @@
-from google_api_search import travel_assistant
-
-lattitude=53.549438522200916
-longtitude=9.985065094736022
-
-if __name__ == "__main__":
-    travel_assistant(lat=lattitude, lng=longtitude, topics="pizza, restaurants", total_n=5)
-
